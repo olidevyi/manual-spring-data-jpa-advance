bdb0284f65760592810d8758268864ee8a8ca5e2
Olidev <jean.yantas@gmail.com>
2023-11-08 15:37:16 -0500

    Proyecto base

---
+++ src/main/java/com/olidev/pe/manualspringdatajpaadvance/ManualSpringDataJpaAdvanceApplication.java
@@ -0,0 +1,13 @@
+package com.olidev.pe.manualspringdatajpaadvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManualSpringDataJpaAdvanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManualSpringDataJpaAdvanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olidev/pe/manualspringdatajpaadvance/ManualSpringDataJpaAdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olidev.pe.manualspringdatajpaadvance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManualSpringDataJpaAdvanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
